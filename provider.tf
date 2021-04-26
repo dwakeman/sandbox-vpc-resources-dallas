0af4145815881f110f6e118e062ef66a50b03517
Dave Wakeman <dwakeman@us.ibm.com>
2021-04-25 20:01:22 -0500

    updates ibm provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = "1.23.2"
+    }
+  }
+}
+
+
 provider "ibm" {
     //generation = var.generation
     region     = var.region
